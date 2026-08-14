0e85edbb2d21a20573f655cee78edffa6c9289f8
Ange Gbocho <50459666+Boomerang23@users.noreply.github.com>
2026-08-14 00:02:23 -0400

    Add Lab 4 functions project

---
+++ Lab4_Ange.Gbocho.py
@@ -0,0 +1,7 @@
+# Lab 4 - Defining and Calling a Function
+
+def introduce_person(name):
+    # Print an introduction line
+    print(f"My name is {name} and I am learning Python.")
+
+introduce_person("Ange")
